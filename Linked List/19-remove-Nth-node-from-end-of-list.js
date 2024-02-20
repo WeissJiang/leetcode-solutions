97121815c8ac70b7ec2db797121165fa9a2aa723
Weiss Jiang <meweissjiang@gmail.com>
2024-02-21 09:44:22 +1100

    fixed 19

--- Linked List/19-remove-Nth-node-from-end-of-list.js
+++ Linked List/19-remove-Nth-node-from-end-of-list.js
@@ -5,6 +5,8 @@ var removeNthFromEnd = function(head, n) {
     let slow = head;
     while(n--) fast = fast.next;
 
+    if(!fast) return head.next;
+
     while(fast.next) {
         fast = fast.next;
         slow = slow.next;
