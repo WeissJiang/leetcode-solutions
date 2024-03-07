9ee11ae5f7fd6603b28734eb96752dc569a05145
Weiss Jiang <meweissjiang@gmail.com>
2024-03-07 19:18:47 +1100

    added 349

---
+++ Array&Hashing/easy/349-intersection-of-two-arrays.js
@@ -0,0 +1,30 @@
+/**
+ * Solution
+ * Given two integer arrays nums1 and nums2, return an array of their intersection. 
+ * Each element in the result must be unique and you may return the result in any order.
+ */
+ 
+function solution(nums1, nums2) {
+    const set = new Set(nums1);
+    const result = [];
+
+    for (let i = 0; i < nums2.length; i++) {
+        const num = nums2[i];
+        if(set.has(num) && !result.includes(num)) {
+            result.push(num);
+        }
+    }
+
+    return result;
+}
+
+
+/**
+ * Test Cases
+ */
+
+// const nums1 = [1,2,2,1], nums2 = [2,2]; // expected: [2]
+const nums1 = [4,9,5], nums2 = [9,4,9,8,4]; // expected: [9,4]s 
+
+const output = solution(nums1, nums2);
+console.log(output)
